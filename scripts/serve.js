e6da5862b9cb9238a837dc942cd19d5e0cfb7b00
destructobeam <destructobeam@stumpedlyfe.com>
2018-12-23 13:08:36 +1300

    some nicer syntax

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,18 +1,20 @@
-const http = require('http');
-const https = require('https');
+import 'source-map-support/register';
 
-const app = require('../src/server').default;
+import http from 'http';
+import https from 'https';
+import app from '../src/server';
+
 const dev = process.env.NODE_ENV !== 'production';
-const ssl = process.env.PROTOCOL || 'http';
+const protocol = process.env.PROTOCOL || 'http';
 const port = process.env.PORT || 3000;
 
 let handler = app.callback();
 let server;
 
-if (ssl) {
-  server = http.createServer(handler);
-} else {
+if (protocol === 'https') {
   server = https.createServer(handler);
+} else {
+  server = http.createServer(handler);
 }
 
 server.listen(port, error => {
@@ -20,17 +22,18 @@ server.listen(port, error => {
     console.log(error);
   }
 
-  console.log('Server started on port ', port);
+  console.log(`Server started on port ${port}`);
 });
 
 if (module.hot) {
   console.log('Server HMR Enabled!');
 
-  module.hot.accept('../src/server', () => {
+  module.hot.accept('../src/server', async () => {
     console.log('Reloading Server');
 
     try {
-      const newHandler = require('../src/server').default.callback();
+      const newApp = await import('../src/server');
+      const newHandler = newApp.default.callback();
 
       server.removeListener('request', handler);
       server.on('request', newHandler);
